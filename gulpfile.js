d9cfb4e51709d660a6240a15610284b91dc064c1
Elena Terehovskaya <len-sh@yandex.ru>
2018-12-01 18:54:26 +0300

    Добавлен picturefill для IE

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task("clean", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**"
+    "source/img/**",
+    "source/js/**"
   ], {
     base: "source"
   })
